62e16c9ee0aad2b940861f6bd6ec0a7ee6229913
Christoph Burgdorf <christoph.burgdorf@bvsn.org>
2014-06-18 16:44:57 +0200

    chore(release): cut 0.1.0 release

---
+++ CHANGELOG.md
@@ -0,0 +1,21 @@
+<a name="0.1.0"></a>
+## 0.1.0 (2014-06-18)
+
+
+#### Bug Fixes
+
+* ***:** rename some seed stuff ([b179beb4](https://github.com/sofa/sofa-state-resolver-service/commit/b179beb4d15587350941426487721c803090cd77))
+* **Gruntfile:** fixes license banner ([f86a8f27](https://github.com/sofa/sofa-state-resolver-service/commit/f86a8f27c6c77236e822451789c089af172ec516))
+* **LICENSE:** fixes naming ([8fc0fb21](https://github.com/sofa/sofa-state-resolver-service/commit/8fc0fb21f48c4410266ace55654c0ae5fd93cdfd))
+
+
+<a name="0.0.0"></a>
+## 0.0.0 (2014-06-18)
+
+
+#### Bug Fixes
+
+* ***:** rename some seed stuff ([b179beb4](https://github.com/sofa/sofa-state-resolver-service/commit/b179beb4d15587350941426487721c803090cd77))
+* **Gruntfile:** fixes license banner ([f86a8f27](https://github.com/sofa/sofa-state-resolver-service/commit/f86a8f27c6c77236e822451789c089af172ec516))
+* **LICENSE:** fixes naming ([8fc0fb21](https://github.com/sofa/sofa-state-resolver-service/commit/8fc0fb21f48c4410266ace55654c0ae5fd93cdfd))
+
---
+++ dist/sofa.stateResolverService.js
@@ -0,0 +1,91 @@
+/**
+ * sofa-state-resolver-service - v0.0.0 - 2014-06-18
+ * http://www.sofa.io
+ *
+ * Copyright (c) 2014 CouchCommerce GmbH (http://www.couchcommerce.com / http://www.sofa.io) and other contributors
+ * THIS SOFTWARE CONTAINS COMPONENTS OF THE SOFA.IO COUCHCOMMERCE SDK (WWW.SOFA.IO).
+ * IT IS PROVIDED UNDER THE LICENSE TERMS OF THE ATTACHED LICENSE.TXT.
+ */
+;(function (sofa, undefined) {
+
+'use strict';
+/* global sofa */
+/**
+ * @name StateResolverService
+ * @class
+ * @namespace sofa.StateResolverService
+ *
+ * @description
+ * `sofa.StateResolverService` is a service to resolve human readable URLs into states that
+ * can be dealed with on an application level.
+ */
+sofa.define('sofa.StateResolverService', function ($q, $http, configService) {
+    var self            = {},
+        statesEndpoint  = configService.get('apiEndpoint') + 'states',
+        storeCode       = configService.get('storeCode'),
+        states          = {};
+
+    /**
+    * @method registerState
+    * @memberof sofa.StateResolverService
+    *
+    * @description
+    * registers a state
+    *
+    * @example
+    * stateResolverService.registerState(state);
+    *
+    * @param {object} state The state to be registered.
+    *
+    */
+    self.registerState = function (state) {
+        states[state.url] = state;
+    };
+
+    /**
+    * @method resolveState
+    * @memberof sofa.StateResolverService
+    *
+    * @description
+    * Takes an URL and resolves it into a state
+    *
+    * @example
+    * stateResolverService
+    *    .resolveState(url)
+    *    .then(function(state){
+    *        //do something with state
+    *    })
+    *
+    * @param {object} deferred The deferred carrying the resolved state.
+    */
+    self.resolveState = function (url) {
+        var deferred = $q.defer();
+
+        if (states[url]) {
+            deferred.resolve(states[url]);
+        }
+        else {
+
+            $http({
+                method: 'POST',
+                url: statesEndpoint,
+                data: {
+                    storeCode: storeCode,
+                    url: url
+                }
+            })
+            .then(function (response) {
+                deferred.resolve(response.data);
+            }, function () {
+                deferred.reject();
+            });
+        }
+
+        return deferred.promise;
+    };
+
+    return self;
+
+});
+
+}(sofa));
