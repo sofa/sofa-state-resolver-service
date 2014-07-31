c8871756f152ea3fb92ee28f35efa2fbf89309e6
Christoph Burgdorf <christoph.burgdorf@bvsn.org>
2014-07-31 10:51:25 +0200

    fix(stateResolver): order parameter correctly

--- src/sofa.stateResolver.js
+++ src/sofa.stateResolver.js
@@ -8,7 +8,7 @@
  * `StateResolver` is used within the`StateResolverService` to resolve a state
  * for a given url. It can easily be overwritten to swap out the resolve strategy.
  */
-sofa.define('sofa.StateResolver', function ($http, $q, configService) {
+sofa.define('sofa.StateResolver', function ($q, $http, configService) {
     var STATES_ENDPOINT = configService.get('apiEndpoint') + 'states';
 
     return function (config) {
